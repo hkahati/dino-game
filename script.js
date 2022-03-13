8af7844c4d1cfe359e22f4eff2c75029e90a47f4
hkahati <hkahatins@gmail.com>
2022-03-13 15:48:58 -0300

    cactus spawn mechanics

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
 let itsJumping = false;
 
 function jump(){
@@ -31,5 +32,29 @@ function handleKeyUp(event){
         }
     }
 }
+
+function createCactus(){
+    const cactus = document.createElement("div");
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 5000;
+
+    cactus.classList.add("cactus");
+    cactus.style.left = cactusPosition + "px";
+    background.appendChild(cactus);
+    
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + "px";
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 
 document.addEventListener('keyup', handleKeyUp);
